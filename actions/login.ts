59657d49e33a35a9a3dd8659586d43fa2b266f64
pastuchenko <pablopastuchenkooo@gmail.com>
2024-07-02 13:55:07 -0300

    register/login concluido

--- actions/login.ts
+++ actions/login.ts
@@ -1,15 +1,40 @@
 "use server";
+
+
 import * as z from "zod"
-
+import { signIn } from "@/auth";
 import { LoginSchema } from "@/schema";
-
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
+import { AuthError } from "next-auth";
 export const login = async (values: z.infer<typeof LoginSchema>) => {
-    const validadeFields = LoginSchema.safeParse(values)
+    const validatedFields = LoginSchema.safeParse(values)
 
-    if (!validadeFields.success) {
+    if (!validatedFields.success) {
         return {error: "Invalid Fields"}
     }
+
     
-    return {success: "Email sent!"}
+    
+    const {email, password} = validatedFields.data
+
+    try {
+        await signIn("credentials", {
+            email,
+            password,
+            redirectTo: DEFAULT_LOGIN_REDIRECT
+        })
+    } catch (error) {
+       // TODO
+       if(error instanceof AuthError) {
+        switch(error.type) {
+            case "CredentialsSignin":
+                return {error: "Invalid Credentials"}
+            default:
+                return {error: "Something went wrong!"}
+        }
+       }
+
+       throw error;
+    }
     
 }
---
+++ routes.ts
@@ -0,0 +1,38 @@
+
+/**
+*An array of routes that are accessible to the public
+* Thesse routes do not require authenticantion
+* @type {string[]}
+*/
+
+
+export const publicRoutes = [
+    "/",
+]
+
+/**
+*An array of routes that are used for authentication
+* Thesse routes will redirect logged in users to /settings
+* @type {string[]}
+*/
+
+export const authRoutes = [
+    "/auth/login",
+    "/auth/register",
+]
+
+/**
+* The prefix for API authentication routes
+* Routes that start with prefix are used for API
+* @type {string}
+*/
+
+
+export const apiAuthPrefix = "/api/auth"
+
+/**
+ * The default redirect path after loggin in
+ * @type {string}
+ */
+
+export const DEFAULT_LOGIN_REDIRECT = "/settings"
